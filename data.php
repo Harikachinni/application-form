5e8013f50f33ad9728c848c54435133c55658640
Harikachinni <30421038+Harikachinni@users.noreply.github.com>
2017-08-21 13:11:13 +0100

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,7 @@
 creating datebase for school dropdown list in sql
 CREATE TABLE 'school'.'name'(`sch_id` INT(10) NOT NULL AUTO_INCREMENT, `sch_name` VARCHAR(30) NOT NULL, 
+username` varchar(30) NOT NULL,
+`email` varchar(30) NOT NULL,
 PRIMARY KEY(`emp_id`)) ENGINE = InnoDB;
 (
 insert into school (sch_id,sch_name) values(0,'oxford school');
